8744e10b1d7eb8cb94204fcf9177a6bdf404621b
Tim Kipp <timkipp.22.developer@gmail.com>
2019-08-14 23:49:21 -0700

    Added generate seed command to migration provider

--- src/Providers/MigrationsProvider.php
+++ src/Providers/MigrationsProvider.php
@@ -3,6 +3,7 @@
 namespace Intersect\Providers;
 
 use Intersect\Providers\AppServiceProvider;
+use Intersect\Database\Migrations\GenerateSeedCommand;
 use Intersect\Database\Migrations\RunMigrationsCommand;
 use Intersect\Database\Migrations\ExportMigrationsCommand;
 use Intersect\Database\Migrations\GenerateMigrationCommand;
@@ -17,6 +18,7 @@ class MigrationsProvider extends AppServiceProvider {
         $app->command('migrations:export', function() use ($app) { return new ExportMigrationsCommand($app->getConnection(), $app->getMigrationPaths()); });
         $app->command('migrations:install', function() use ($app) { return new InstallMigrationsCommand($app->getConnection()); });
         $app->command('migrations:generate', function() use ($app) { return new GenerateMigrationCommand($app->getMigrationsPath()); });
+        $app->command('migrations:generate-seed', function() use ($app) { return new GenerateSeedCommand($app->getMigrationsPath()); });
         $app->command('migrations:run', function() use ($app) { return new RunMigrationsCommand($app->getConnection(), $app->getMigrationPaths()); });
     }
 
