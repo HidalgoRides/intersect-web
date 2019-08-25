07efaacc4219f26832f704de11b0a5eb71354a22
Tim Kipp <timkipp.22.developer@gmail.com>
2019-08-24 18:26:30 -0700

    Removed references to old InstallMigrationsCommand

--- src/Providers/MigrationsProvider.php
+++ src/Providers/MigrationsProvider.php
@@ -7,7 +7,6 @@ use Intersect\Database\Migrations\Commands\GenerateSeedCommand;
 use Intersect\Database\Migrations\Commands\RunMigrationsCommand;
 use Intersect\Database\Migrations\Commands\ExportMigrationsCommand;
 use Intersect\Database\Migrations\Commands\GenerateMigrationCommand;
-use Intersect\Database\Migrations\Commands\InstallMigrationsCommand;
 use Intersect\Database\Migrations\Commands\RollbackMigrationsCommand;
 
 class MigrationsProvider extends AppServiceProvider {
@@ -17,7 +16,6 @@ class MigrationsProvider extends AppServiceProvider {
         $app = $this->app;
 
         $app->command('migrations:export', function() use ($app) { return new ExportMigrationsCommand($app->getConnection(), $app->getMigrationPaths()); });
-        $app->command('migrations:install', function() use ($app) { return new InstallMigrationsCommand($app->getConnection()); });
         $app->command('migrations:generate', function() use ($app) { return new GenerateMigrationCommand($app->getMigrationsPath()); });
         $app->command('migrations:generate-seed', function() use ($app) { return new GenerateSeedCommand($app->getMigrationsPath()); });
         $app->command('migrations:rollback', function() use ($app) { return new RollbackMigrationsCommand($app->getConnection(), $app->getMigrationPaths()); });
