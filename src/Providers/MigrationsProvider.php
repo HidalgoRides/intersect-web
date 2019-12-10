1e92584fda3dc11157b2a2b1cb5430571f298539
Tim Kipp <timkipp.22.developer@gmail.com>
2019-12-09 16:53:24 -0800

    Added migrations:reset command and fixed test file dependencies

--- src/Providers/MigrationsProvider.php
+++ src/Providers/MigrationsProvider.php
@@ -7,6 +7,7 @@ use Intersect\Database\Migrations\Commands\GenerateSeedCommand;
 use Intersect\Database\Migrations\Commands\RunMigrationsCommand;
 use Intersect\Database\Migrations\Commands\ExportMigrationsCommand;
 use Intersect\Database\Migrations\Commands\GenerateMigrationCommand;
+use Intersect\Database\Migrations\Commands\ResetMigrationsCommand;
 use Intersect\Database\Migrations\Commands\RollbackMigrationsCommand;
 
 class MigrationsProvider extends AppServiceProvider {
@@ -18,6 +19,7 @@ class MigrationsProvider extends AppServiceProvider {
         $app->command('migrations:export', function() use ($app) { return new ExportMigrationsCommand($app->getConnection(), $app->getMigrationPaths()); });
         $app->command('migrations:generate', function() use ($app) { return new GenerateMigrationCommand($app->getMigrationsPath()); });
         $app->command('migrations:generate-seed', function() use ($app) { return new GenerateSeedCommand($app->getMigrationsPath()); });
+        $app->command('migrations:reset', function() use ($app) { return new ResetMigrationsCommand($app->getConnection(), $app->getMigrationPaths()); });
         $app->command('migrations:rollback', function() use ($app) { return new RollbackMigrationsCommand($app->getConnection(), $app->getMigrationPaths()); });
         $app->command('migrations:run', function() use ($app) { return new RunMigrationsCommand($app->getConnection(), $app->getMigrationPaths()); });
     }
